c7124f0cc95fe67c3334aacfde0784aa6923144a
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2017-05-26 01:05:35 +0900

    이동컴포넌트 추가

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    private Transform tr;
+    public float moveSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("H=" + h.ToString());
+        Debug.Log("V=" + v.ToString());
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        tr.Translate(moveDir.normalized * Time.deltaTime * moveSpeed, Space.Self);
+    }
+}

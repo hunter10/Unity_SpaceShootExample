0682c2e260088c8d6d8b720cf00877113b4041f2
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2017-06-01 00:19:57 +0900

    드럼통추가

--- Assets/02.Scripts/WallCtrl.cs
+++ Assets/02.Scripts/WallCtrl.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class WallCtrl : MonoBehaviour {
 
+    public GameObject sparkEffect;
+
     private void OnCollisionEnter(Collision coll)
     {
         if(coll.collider.tag == "BULLET")
         {
+            GameObject spark = (GameObject)Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+
+            Destroy(spark, spark.GetComponent<ParticleSystem>().duration + 0.2f);
+
             Destroy(coll.gameObject);
         }
     }

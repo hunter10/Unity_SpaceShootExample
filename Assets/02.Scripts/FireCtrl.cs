aa0c2ca2e8ae14ae8e96080040a798fa16c0fec4
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2017-06-02 00:17:13 +0900

    총구화염 추가, 드럼통 추가

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour {
 
     public GameObject bullet;
     public Transform firePos;
+    public AudioClip fireSfx;
+    private AudioSource source = null;
+    public MeshRenderer muzzleFlash;
 
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        muzzleFlash.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if(Input.GetMouseButtonDown(0))
         {
             Fire();
@@ -18,10 +28,27 @@ public class FireCtrl : MonoBehaviour {
     void Fire()
     {
         CreateBullet();
+        source.PlayOneShot(fireSfx, 0.9f);
+        StartCoroutine(this.ShowMuzzleFlash());
     }
 
     void CreateBullet()
     {
         Instantiate(bullet, firePos.position, firePos.rotation);
+    }
+
+    IEnumerator ShowMuzzleFlash()
+    {
+        float scale = Random.Range(1.0f, 2.0f);
+        muzzleFlash.transform.localScale = Vector3.one * scale;
+
+        Quaternion rot = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        muzzleFlash.transform.localRotation = rot;
+
+        muzzleFlash.enabled = true;
+
+        yield return new WaitForSeconds(Random.Range(0.05f, 0.07f));
+
+        muzzleFlash.enabled = false;
     }
 }
